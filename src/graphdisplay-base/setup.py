af689e73345790b824f6e9970d4648dfbc8fa3fc
Beto <100471939@alumnos.uc3m.es>
2023-05-03 22:36:13 +0200

    Setup con información actualizada

--- src/graphdisplay-base/setup.py
+++ src/graphdisplay-base/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '0.0.3' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
+VERSION = '0.2.0' #Muy importante, deberéis ir cambiando la versión de vuestra librería según incluyáis nuevas funcionalidades
 PACKAGE_NAME = 'graphdisplay' #Debe coincidir con el nombre de la carpeta
 AUTHOR = 'Alberto Penas Díaz' #Modificar con vuestros datos
 AUTHOR_EMAIL = 'albertopenasdiaz@gmail.com' #Modificar con vuestros datos
@@ -17,7 +17,7 @@ LONG_DESC_TYPE = "text/markdown"
 
 #Paquetes necesarios para que funcione la libreía. Se instalarán a la vez si no lo tuvieras ya instalado
 INSTALL_REQUIRES = [
-      'tk'
+      'tk==0.1.1'
       ]
 
 setup(
@@ -29,8 +29,15 @@ setup(
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     url=URL,
-    install_requires=INSTALL_REQUIRES,
+    #install_requires=INSTALL_REQUIRES,
     license=LICENSE,
     packages=find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7']
 )
