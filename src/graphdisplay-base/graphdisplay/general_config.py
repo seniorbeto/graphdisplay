adaab95dd5983fe4165d124eb2b50f58bc74ef33
100471939 <100471939@alumnos.uc3m.es>
2023-05-07 17:27:19 +0200

    Temas

--- src/graphdisplay-base/graphdisplay/general_config.py
+++ src/graphdisplay-base/graphdisplay/general_config.py
@@ -13,11 +13,67 @@ THEMES = {
         "FRAME_COLOR": "#6aaade",
         "AUTHOR_NAME_COLOR": "#0b2d47"
     },
-    "NEKOS":{
-        "BUTTON_COLOR": "#e7a2fc",
-        "VERTEX_COLOR": "#e4bce6",
-        "BACKGROUND_CANVAS_COLOR": "#ebc3f7",
-        "FRAME_COLOR": "#d574f2",
-        "AUTHOR_NAME_COLOR": "#d574f2"
+    "DARK":{
+        "BUTTON_COLOR": "#6a6a7d",
+        "VERTEX_COLOR": "#7d7d96",
+        "BACKGROUND_CANVAS_COLOR": "#404147",
+        "FRAME_COLOR": "#272729",
+        "AUTHOR_NAME_COLOR": "#c0c0f0"
+    },
+    "JESUSFUCKINGCHRIST":{
+        "BUTTON_COLOR": "#fffb00",
+        "VERTEX_COLOR": "#fffb00",
+        "BACKGROUND_CANVAS_COLOR": "#fffb00",
+        "FRAME_COLOR": "#fffb00",
+        "AUTHOR_NAME_COLOR": "#000000"
+    },
+    "PURPLE":{
+        "BUTTON_COLOR": "#f2c8fa",
+        "VERTEX_COLOR": "#e7cdf7",
+        "BACKGROUND_CANVAS_COLOR": "#e4c3fa",
+        "FRAME_COLOR": "#d899f7",
+        "AUTHOR_NAME_COLOR": "#6d1aa3"
+    },
+    "GREEN":{
+        "BUTTON_COLOR": "#B5FFC7",
+        "VERTEX_COLOR": "#A1FDB3",
+        "BACKGROUND_CANVAS_COLOR": "#BFFFCF",
+        "FRAME_COLOR": "#80FF99",
+        "AUTHOR_NAME_COLOR": "#05331F"
+    },
+    "DARKGREEN":{
+        "BUTTON_COLOR": "#173F2D",
+        "VERTEX_COLOR": "#215C41",
+        "BACKGROUND_CANVAS_COLOR": "#163623",
+        "FRAME_COLOR": "#0C2B1A",
+        "AUTHOR_NAME_COLOR": "#FFFFFF"
+    },
+    "UBUNTU":{
+        "BUTTON_COLOR": "#b35625",
+        "VERTEX_COLOR": "#b35625",
+        "BACKGROUND_CANVAS_COLOR": "#404040",
+        "FRAME_COLOR": "#303030",
+        "AUTHOR_NAME_COLOR": "#FFFFFF"
+    },
+    "LA NOCHE ESTRELLADA":{
+        "BUTTON_COLOR": "#9bafaa",
+        "VERTEX_COLOR": "#7b94a4",
+        "BACKGROUND_CANVAS_COLOR": "#557491",
+        "FRAME_COLOR": "#242d34",
+        "AUTHOR_NAME_COLOR": "#d5bc6a"
+    },
+    "LA GRAN OLA":{
+        "BUTTON_COLOR": "#a6a49b",
+        "VERTEX_COLOR": "#e7e2d2",
+        "BACKGROUND_CANVAS_COLOR": "#8599a4",
+        "FRAME_COLOR": "#4a5f79",
+        "AUTHOR_NAME_COLOR": "#d5bc6a"
+    },
+    "ANTARTICA":{
+        "BUTTON_COLOR": "#9BA4B5",
+        "VERTEX_COLOR": "#9BA4B5",
+        "BACKGROUND_CANVAS_COLOR": "#394867",
+        "FRAME_COLOR": "#212A3E",
+        "AUTHOR_NAME_COLOR": "#83764F"
     }
 }
