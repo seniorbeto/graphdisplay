5c9838d37da754b32a5400b656d03455cb01d77b
100471939 <100471939@alumnos.uc3m.es>
2023-05-16 19:20:13 +0200

    Ventana "About" mejorada. Ajustado de temas.

--- src/graphdisplay-base/graphdisplay/about_win_manager.py
+++ src/graphdisplay-base/graphdisplay/about_win_manager.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import turtle
-from time import sleep
+import webbrowser
 from .general_config import *
 
 class AboutWindow(tk.Toplevel):
@@ -8,15 +8,24 @@ class AboutWindow(tk.Toplevel):
         super().__init__(root)
         self.__graphgui = graphgui
         self.title("About")
-        self.geometry("400x400")
+        self.geometry("400x430")
         self.resizable(False, False)
+        self.configure(bg=self.__graphgui._FRAME_COLOR, width=400, height=430)
+
+        buton = tk.Button(self, text="Contribute", bg=self.__graphgui._BUTTON_COLOR, command=self.__open_github, bd=0)
+        buton.pack(side=tk.BOTTOM)
 
         self.canvas = tk.Canvas(self, width=400, height=400, bg=self.__graphgui._BACKGROUND_CANVAS_COLOR)
         self.canvas.pack()
         self.turtle = turtle.RawTurtle(self.canvas, visible=False)
         self.canvas.configure(bg=self.__graphgui._BACKGROUND_CANVAS_COLOR)
         self.turtle.speed(0)
-        text = self.canvas.create_text(0, -150, text="GraphDisplay", font=("Courier", 20),
+        text = "GraphDisplay v"+VERSION
+        self.canvas.create_text(0, -150, text=text, font=("Courier", 20),
+                                fill=self.__graphgui._AUTHOR_NAME_COLOR)
+        self.canvas.create_text(0, 147, text="An open source proyect made", font=("Courier", 13),
+                                fill=self.__graphgui._AUTHOR_NAME_COLOR)
+        self.canvas.create_text(0, 165, text="by and for students!", font=("Courier", 13),
                                 fill=self.__graphgui._AUTHOR_NAME_COLOR)
         for i in range(92):
             if i % 2 == 0:
@@ -27,5 +36,8 @@ class AboutWindow(tk.Toplevel):
             self.turtle.forward(i+1+5)
             self.turtle.right(91)
 
+    def __open_github(self):
+        webbrowser.open("https://github.com/seniorbeto/graphdisplay")
 
-#        self.canvas.configure(bg=self.__graphgui._BACKGROUND_CANVAS_COLOR)
+    def __open_twitter(self):
+        webbrowser.open("https://twitter.com/seniorbeto__")
